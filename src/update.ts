741c858ad14a12d8a261c487a376b2b16aa5c16e
Anik Barua <anikbb05@gmail.com>
2024-03-07 21:26:43 +0600

    find update and update many

--- src/update.ts
+++ src/update.ts
@@ -1,30 +1,30 @@
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
-// const main = async () => {
-//   // single update
-//   // update id=7 and set new author_name john doe updated
-//   const updatePost = await prisma.post.update({
-//     where: {
-//       id: 7,
-//     },
-//     data: {
-//       author_name: "John doe updated",
-//     },
-//   });
-//   //   console.log(updatePost);
+const main = async () => {
+  // single update
+  // update id=7 and set new author_name john doe updated
+  const updatePost = await prisma.post.update({
+    where: {
+      id: 7,
+    },
+    data: {
+      author_name: "John doe updated",
+    },
+  });
+  //   console.log(updatePost);
 
-//   //   update multiple row
-//   const updatePosts = await prisma.post.updateMany({
-//     where: {
-//       author_name: "Sarah smith blue",
-//     },
-//     data: {
-//       publish: true,
-//     },
-//   });
-//   console.log(updatePosts);
-// };
+  //   update multiple row
+  const updatePosts = await prisma.post.updateMany({
+    where: {
+      author_name: "Sarah smith blue",
+    },
+    data: {
+      publish: true,
+    },
+  });
+  console.log(updatePosts);
+};
 
-// main();
+main();
